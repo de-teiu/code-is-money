69cbae6c1d97c5edd444887018f53caa038fea7f
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2017-08-12 23:29:48 +0900

    メニューに[CodeIsMoney]の項目を追加し、そこから拡張機能の表示/非表示を切り替えられるようにした

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 define(function (require, exports, module) {
 
+    const CommandManager = brackets.getModule("command/CommandManager");
+    const Menus = brackets.getModule("command/Menus");
+    const WorkspaceManager = brackets.getModule("view/WorkspaceManager");
     const ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
     const infocusExtPath = ExtensionUtils.getModulePath(module);
+    const AppInit = brackets.getModule("utils/AppInit");
+
+    const panelHtml = require("text!panel.html");
+    var panel;
+
+    const CODEISMONEY_SHOW = "codeismoney.show";
+    const COMMAND_ID = "CodeIsMoney";
 
     const ignoreKeyList = [8, 37, 38, 39, 40, 46];
     const seFileName = 'coin01.ogg';
@@ -12,6 +22,8 @@ define(function (require, exports, module) {
     for (var i = 0; i < fileSet; i++) {
         se.push(new Audio(infocusExtPath + seFileName));
     }
+
+
 
     /**
      * キー入力時のイベント
@@ -40,6 +52,31 @@ define(function (require, exports, module) {
         document.addEventListener("keydown", KeyDownFunc);
 
     }
+
+    function handle() {
+        if (panel.isVisible()) {
+            panel.hide();
+            CommandManager.get(CODEISMONEY_SHOW).setChecked(false);
+        } else {
+            panel.show();
+            CommandManager.get(CODEISMONEY_SHOW).setChecked(true);
+        }
+    }
+
+    AppInit.appReady(function () {
+        ExtensionUtils.loadStyleSheet(module, "style.css");
+        CommandManager.register(COMMAND_ID, CODEISMONEY_SHOW, handle);
+        var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
+        menu.addMenuItem(CODEISMONEY_SHOW, "Ctrl-Alt-?");
+        panel = WorkspaceManager.createBottomPanel(CODEISMONEY_SHOW, $(panelHtml), 48);
+
+        // Close the panel by clicking on X
+        /*
+        $('#infocus-panel .close').click(function () {
+            handle();
+        });
+        */
+    });
 });
 
 //--------
