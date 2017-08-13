2ebd6c5106e283fe7aa04b4d8cf54d1b5af8d83a
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2017-08-13 19:48:02 +0900

    ・ファイルから読み込んだ金額を初期表示するようにした ・金額表示部分を実装(デザインの調整は後日)

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ define(function (require, exports, module) {
     const infocusExtPath = ExtensionUtils.getModulePath(module);
     const AppInit = brackets.getModule("utils/AppInit");
 
+    const CurrentDat = 'current.dat';
+
     const panelHtml = require("text!panel.html");
     var panel;
 
@@ -16,6 +18,8 @@ define(function (require, exports, module) {
     const ignoreKeyList = [8, 37, 38, 39, 40, 46];
     const seFileName = 'coin01.ogg';
     const fileSet = 20;
+
+    var currentGold = 0;
 
     var seCount = 0;
     var se = [];
@@ -29,13 +33,20 @@ define(function (require, exports, module) {
      * キー入力時のイベント
      */
     function KeyDownFunc(e) {
+        if (!panel.isVisible()) {
+            return;
+        }
+
         var keyCode = e.keyCode;
 
         //特定のキーが入力された場合は無視
         if (ignoreKeyList.indexOf(keyCode) >= 0) {
             return;
         }
+
         playSound();
+        currentGold++;
+        document.getElementById("text-current-your-money").innerHTML = currentGold.toLocaleString();
     }
 
     function playSound() {
@@ -53,20 +64,44 @@ define(function (require, exports, module) {
 
     }
 
+
+    function readCurrent() {
+        var temp = require('text!current.dat');
+        if (isNaN(temp)) {
+            temp = 0;
+        } else {
+            temp = Number(temp);
+        }
+        return temp;
+    }
+    /*
+        function writeCurrent() {
+            var fs = require('fs');
+            fs.writeFile('DataFile', currentGold, function (err) {
+                console.log(err);
+            });
+        }
+    */
     function handle() {
         if (panel.isVisible()) {
+            //writeCurrent();
             panel.hide();
             CommandManager.get(CODEISMONEY_SHOW).setChecked(false);
         } else {
+            //ここでcurrent.datから現在の金額を読み込む
+            currentGold = readCurrent();
+            document.getElementById("text-current-your-money").innerHTML = currentGold.toLocaleString();
             panel.show();
             CommandManager.get(CODEISMONEY_SHOW).setChecked(true);
         }
     }
 
+
+
     AppInit.appReady(function () {
         ExtensionUtils.loadStyleSheet(module, "style.css");
         CommandManager.register(COMMAND_ID, CODEISMONEY_SHOW, handle);
-        var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
+        var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
         menu.addMenuItem(CODEISMONEY_SHOW, "Ctrl-Alt-?");
         panel = WorkspaceManager.createBottomPanel(CODEISMONEY_SHOW, $(panelHtml), 48);
 
@@ -77,6 +112,8 @@ define(function (require, exports, module) {
         });
         */
     });
+
+
 });
 
 //--------
