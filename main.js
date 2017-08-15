085914e119bd681f1a054f86118e33f9518ac3f8
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2017-08-15 22:51:56 +0900

    キータイプ時の金額増加のアニメーションを実装。効果音を変更。

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 define(function (require, exports, module) {
+    require('jquery.easing.1.3');
 
     const CommandManager = brackets.getModule("command/CommandManager");
     const Menus = brackets.getModule("command/Menus");
@@ -18,7 +19,8 @@ define(function (require, exports, module) {
     const COMMAND_ID = "CodeIsMoney";
 
     const ignoreKeyList = [8, 37, 38, 39, 40, 46];
-    const seFileName = 'coin01.ogg';
+    const seFileName = 'coin00.ogg';
+    const imageFileName = 'coin.gif';
     const fileSet = 20;
 
     var currentGold = 0;
@@ -30,6 +32,52 @@ define(function (require, exports, module) {
     }
 
 
+    var coinNum = 0;
+
+    function setCoinObj() {
+        var objId = 'coin' + coinNum;
+        coinNum++;
+        var imagePath = infocusExtPath + imageFileName;
+        $('#panel-current-your-money')
+            .append(
+                '<img src="' +
+                imagePath +
+                '" alt="" id="' + objId + '" width="' + 24 + '" height="' + 24 + '">');
+
+        var tempLeft = Math.floor(Math.random() * 201) - 24;
+        var tempTop = (-1) * Math.floor(Math.random() * 101) - 50;
+        $('#' + objId).css({
+            width: 24 + 'px',
+            height: 24 + 'px',
+            overflow: 'hidden',
+            position: 'absolute',
+            display: 'inline',
+            top: tempTop + 'px',
+            left: tempLeft + 'px',
+        });
+
+        $('#' + objId).animate({
+            top: 0,
+            left: 200 - 24
+        }, {
+            duration: 700,
+            specialEasing: {
+                top: 'easeInQuad',
+                left: 'easeInQuad'
+            },
+            done: function (animation, goToEnd) {
+                playSound();
+                currentGold++;
+                document.getElementById("text-current-your-money").innerHTML = currentGold.toLocaleString();
+
+                $('#' + objId).remove();
+            },
+            fail: function (animation, goToEnd) {
+
+            },
+            always: function (animation, goToEnd) {}
+        });
+    }
 
     /**
      * キー入力時のイベント
@@ -46,9 +94,8 @@ define(function (require, exports, module) {
             return;
         }
 
-        playSound();
-        currentGold++;
-        document.getElementById("text-current-your-money").innerHTML = currentGold.toLocaleString();
+
+        setCoinObj();
     }
 
     function playSound() {
@@ -60,16 +107,22 @@ define(function (require, exports, module) {
     }
 
 
-    if (document.addEventListener) {
-        //イベントリスナの設定
-        document.addEventListener("keydown", KeyDownFunc);
-
-    }
+    //キー入力時イベントリスナの設定
+    document.addEventListener("keydown", KeyDownFunc);
+    //終了時イベントリスナの設定
+    window.addEventListener('beforeunload', function (e) {
+        writeCurrent();
+    }, false);
 
     function writeCurrent() {
         var targetFile = FileSystem.getFileForPath(ProjectManager.getProjectRoot().fullPath + CurrentDat);
         targetFile.write(String(currentGold));
     }
+
+    var unload = function () {
+        writeCurrent();
+    };
+    exports.unload = unload;
 
 
     function handle() {
